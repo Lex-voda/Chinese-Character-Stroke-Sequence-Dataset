1c8dee1b629eeb8bb04a660180fd11f4e1629b63
Ex-voda <lsh301705@gmail.com>
2023-12-23 10:41:46 +0800

    final

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ import cairosvg
 import cv2
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
+import numpy as np
+from scipy.sparse import csr_matrix
 
 def generate_svg(svg_save_path, path_data):
     dwg = svgwrite.Drawing(svg_save_path, profile='tiny', size=(1024, 1024))
@@ -19,16 +21,19 @@ def svg_to_png(svg_file_path, jpg_file_path):
         svg_content = svg_file.read()
     cairosvg.svg2png(bytestring=svg_content, write_to=jpg_file_path)
 
-def convert_png_to_binary_jpg(png_path, jpg_path, value):
+def convert_png_to_binary_npz(png_path, npz_path, value):
     img = cv2.imread(png_path, cv2.IMREAD_UNCHANGED)
     alpha_channel = img[:, :, 3]
     img[alpha_channel != 0] = [1,1,1, 0]
-    img = img[:,:,:]
+    # img = img[:,:,:]
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     _, img_binary = cv2.threshold(img_gray, 0, value, cv2.THRESH_BINARY)
 
-    cv2.imwrite(jpg_path, img_binary, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    # 将二值图像转换为稀疏矩阵
+    sparse_matrix = csr_matrix(img_binary)
+    np.savez_compressed(npz_path, data=sparse_matrix.data, indices=sparse_matrix.indices,
+                        indptr=sparse_matrix.indptr, shape=sparse_matrix.shape)
 
 def process_strokes(graphic, fort_category, i):
     folder_name = "chinese_kaiti_{:04d}".format(i + 1)
@@ -42,11 +47,11 @@ def process_strokes(graphic, fort_category, i):
     for j, stroke in enumerate(strokes):
         svg_file_name = 'data/svg/' + folder_name + '/' + str(j) + '.svg'
         png_file_name = 'data/png/' + folder_name + '/' + str(j) + '.png'
-        jpg_file_name = 'data/jpg/' + folder_name + '/' + str(j) + '.jpg'
+        npz_file_name = 'data/npz/' + folder_name + '/' + str(j) + '.npz'
 
         generate_svg(svg_file_name, stroke)
         svg_to_png(svg_file_name, png_file_name)
-        convert_png_to_binary_jpg(png_file_name, jpg_file_name, category_id[j])
+        convert_png_to_binary_npz(png_file_name, npz_file_name, category_id[j])
 
 
 def run():
